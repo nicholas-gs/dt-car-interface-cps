db3aab78a5fa88b4580885daa7b7a30c74f84269
nicholas-gs <nicholasganshyan@gmail.com>
2023-02-02 12:33:48 +0800

    add OOD alert and default passthrough
    Signed-off-by: nicholas-gs <nicholasganshyan@gmail.com>

--- dagu_car/dagu_car/gatekeeper_node.py
+++ dagu_car/dagu_car/gatekeeper_node.py
@@ -3,6 +3,7 @@
 import rclpy
 
 from rclpy.node import Node
+from dt_ood_interfaces_cps.msg import OODAlert
 from dt_interfaces_cps.msg import BoolStamped, Twist2DStamped
 
 
@@ -13,7 +14,9 @@ class GatekeeperNode(Node):
     def __init__(self, node_name: str):
         super().__init__(node_name)
 
-        self.activated = False
+        self.get_launch_params()
+
+        self.ood_alert = False
 
         # Subscribers
         self.sub_car_cmds = self.create_subscription(
@@ -26,6 +29,11 @@ class GatekeeperNode(Node):
             "~/activate",
             self.cb_start,
             1)
+        self.sub_ood_alert = self.create_subscription(
+            OODAlert,
+            "~/ood_alert",
+            self.cb_ood_alert,
+            1)
 
         # Publisher
         self.pub_car_cmd = self.create_publisher(
@@ -33,16 +41,38 @@ class GatekeeperNode(Node):
             "~/out_car_cmd",
             1)
 
-        self.get_logger().info(f"Activated with passthrough: {self.activated}")
+        self.get_logger().info(f"Activated with passthrough: {self.activated} \
+OOD Alert: {self.ood_alert}")
+
+        if not self.activated:
+            self.get_logger().warn(
+                f"Wheel commands not passthrough by default!")
+
+    def get_launch_params(self):
+        self.declare_parameter("default_passthrough")
+        self.activated = self.get_parameter("default_passthrough")\
+            .get_parameter_value().bool_value
 
     def cb_car_cmd(self, wheels_cmd: Twist2DStamped):
         """Callback for car commands."""
-        if self.activated:
+        if self.activated and not self.ood_alert:
             self.pub_car_cmd.publish(wheels_cmd)
 
     def cb_start(self, msg: BoolStamped):
         self.activated = msg.data
         self.get_logger().info(f"Gatekeeper activated: {self.activated}")
+
+    def cb_ood_alert(self, msg: OODAlert):
+        _alert = msg.ood
+
+        # If transition from no OOD alert to OOD alert
+        if not self.ood_alert and _alert:
+            self.get_logger().warn("OOD alert triggered!")
+        # If transition from OOD alert to no alert
+        elif self.ood_alert and not _alert:
+            self.get_logger().warn("OOD alert untriggered")
+
+        self.ood_alert = _alert
 
 
 def main(args=None):
